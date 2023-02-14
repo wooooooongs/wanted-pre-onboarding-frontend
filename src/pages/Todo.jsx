a0e9271930d0750531717ee6c64a9c5a2e2f95df
wooooooongs <pires11233@gmail.com>
2023-02-15 01:05:43 +0900

    :sparkles: feat(Todo): Todo 입력 후 추가 버튼 클릭 시 새로운 Todo 추가 구현

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Todo = () => {
@@ -10,6 +10,31 @@ const Todo = () => {
     if (!ACCESS_TOKEN) navigate('/signin');
   }, [navigate]);
 
+  const [todoValue, setTodoValue] = useState('');
+  const [todos, setTodos] = useState([
+    {
+      todo: '카구리 사기',
+      completed: false,
+    },
+    {
+      todo: '카구리 먹기',
+      completed: false,
+    },
+  ]);
+
+  const handleInputChange = (e) => {
+    setTodoValue(e.target.value);
+  };
+
+  const handleInputSubmit = (e) => {
+    e.preventDefault();
+
+    if (todoValue) {
+      setTodos([...todos, { todo: todoValue, completed: false }]);
+      setTodoValue('');
+    }
+  };
+
   return (
     <>
       <div className='w-full justify-center flex gap-12 mt-[8vh] pb-7'>
@@ -19,10 +44,14 @@ const Todo = () => {
       <div className='w-[15rem] mx-auto mt-[6rem]'>
         <div className='flex mb-5'>
           <input
+            value={todoValue}
+            onChange={handleInputChange}
+            placeholder='카구리 사기'
             className='w-full rounded-sm border-[1.5px] border-slate-500 p-1.5 drop-shadow-md'
             data-testid='new-todo-input'
           />
           <button
+            onClick={handleInputSubmit}
             className='rounded-sm w-[5rem]'
             data-testid='new-todo-add-button'>
             추가
@@ -30,18 +59,14 @@ const Todo = () => {
         </div>
 
         <ul>
-          <li>
-            <label>
-              <input type='checkbox' />
-              <span>TODO 1</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type='checkbox' />
-              <span>TODO 1</span>
-            </label>
-          </li>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              <label className='mr-4'>
+                <input className='mr-2' type='checkbox' />
+                <span>{todo.todo}</span>
+              </label>
+            </li>
+          ))}
         </ul>
       </div>
     </>
