3d4f11068d005c7f1c12fee2681b9ceebeed0999
wooooooongs <pires11233@gmail.com>
2023-02-14 22:12:42 +0900

    :sparkles: feat(auth): 이메일, 패스워드 검증 기능 구현

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from './pages/Root';
 import NotFound from './pages/NotFound';
-import Authentication from './pages/Authenticationcopy';
+import Authentication from './pages/Authentication';
 
 const router = createBrowserRouter([
   {
--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Authentication = (props) => {
   const { authType } = props;
   const titleType = {
     signUp: 'Sign Up',
     signIn: 'Sign In',
+  };
+
+  const navigate = useNavigate();
+
+  const [emailValue, setEmailValue] = useState('');
+  const [emailValidation, setEmailValidation] = useState(false);
+  const [pwdValue, setPwdValue] = useState('');
+  const [PwdValidation, setPwdValidation] = useState(false);
+  const [validationStatus, setValidationStatus] = useState(false);
+
+  useEffect(() => {
+    emailValue.split('').includes('@')
+      ? setEmailValidation(true)
+      : setEmailValidation(false);
+
+    pwdValue.length >= 8 ? setPwdValidation(true) : setPwdValidation(false);
+  }, [emailValue, pwdValue]);
+
+  useEffect(() => {
+    emailValidation && PwdValidation
+      ? setValidationStatus(true)
+      : setValidationStatus(false);
+  }, [emailValidation, PwdValidation]);
+
+  const testHandleSubmit = () => {
+    navigate('/');
   };
 
   return (
@@ -19,6 +46,7 @@ const Authentication = (props) => {
           </div>
           <input
             type='email'
+            onChange={(e) => setEmailValue(e.target.value)}
             className='w-full rounded-sm border-[1.5px] border-slate-500 p-2.5 drop-shadow-md'
             placeholder='test@testemail.com'
           />
@@ -29,12 +57,16 @@ const Authentication = (props) => {
           </div>
           <input
             type='password'
+            onChange={(e) => setPwdValue(e.target.value)}
             className='w-full rounded-sm border-[1.5px] border-slate-500 p-2.5 drop-shadow-md'
             placeholder='12345678'
           />
         </div>
-        <button className='ml-auto border-[1.5px] border-slate-500 rounded-sm w-[4rem] h-[2.5rem] drop-shadow-md'>
-          <span>Done</span>
+        <button
+          disabled={!validationStatus}
+          onClick={testHandleSubmit}
+          className='ml-auto border-[1.5px] border-slate-500 rounded-sm w-[4rem] h-[2.5rem] drop-shadow-md'>
+          <span className={!validationStatus ? 'opacity-50' : ''}>Done</span>
         </button>
       </div>
     </>
